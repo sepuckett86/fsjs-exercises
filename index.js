14177c10186ab43cc4ad94c6390855e55d1b583b
sepuckett86 <sepuckett86@gmail.com>
2019-07-04 15:10:02 -0700

    finish all three

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const Animal = function(name, species) {
+// AnimalConstructor
+const AnimalConstructor = function(name, species) {
   this.name = name;
   this.species = species;
 };
 
-Animal.prototype.speak = function() {
+AnimalConstructor.prototype.speak = function() {
   console.log(`Hi, I'm ${this.name} and my species is ${this.species}`);
 };
 
-const sloth = new Animal('Andy', 'sloth');
-
+const sloth = new AnimalConstructor('Andy', 'sloth');
 sloth.speak();
+
+// AnimalFactory
+const AnimalFactory = function(name, species) {
+  return {
+    name,
+    species,
+    speak() {
+      console.log(`Hi, I'm ${this.name} and my species is ${this.species}`);
+    }
+  };
+};
+
+const manatee = new AnimalFactory('Veteran George', 'manatee');
+manatee.speak();
+
+// AnimalClass
+class AnimalClass {
+  constructor(name, species) {
+    this.name = name;
+    this.species = species;
+  }
+
+  speak() {
+    console.log(`Hi, I'm ${this.name} and my species is ${this.species}`);
+  }
+}
+
+const meerkat = new AnimalClass('Annabelle', 'meerkat');
+meerkat.speak();
