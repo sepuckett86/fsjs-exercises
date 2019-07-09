362b9be07ebc09c7cf0710713feb06d6e6a7dd5c
sepuckett86 <sepuckett86@gmail.com>
2019-07-09 13:37:52 -0700

    update

--- ee.test.js
+++ ee.test.js
@@ -2,9 +2,7 @@ const EventEmitter = require('events');
 
 it('emits and listens to events', done => {
   const ee = new EventEmitter();
-  ee.on('we did it', () => {
-    done();
-  });
+  ee.on('we did it', done);
   ee.emit('we did it');
 });
 
