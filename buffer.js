8aa171863b3771ec68f075109aa14310e4ac1041
sepuckett86 <sepuckett86@gmail.com>
2019-07-09 11:09:55 -0700

    working on it

--- buffer.js
+++ buffer.js
@@ -1,5 +1,11 @@
-function makeBuff(str) {
-  return Buffer.from(str);
+function makeBuff(hex) {
+  return Buffer.from(hex);
 }
+
+const smile = makeBuff([0xF0, 0x9F, 0x98, 0x81]);
+
+console.log([0xF0, 0x9F, 0x98, 0x81]);
+console.log(smile[0]);
+console.log(smile.map(letter => letter.toString(2)));
 
 module.exports = makeBuff;
--- buffer.test.js
+++ buffer.test.js
@@ -1,11 +1,11 @@
 const makeBuff = require('./buffer');
 
 describe('myBuff', () => {
-  const buffer = makeBuff('string');
+  const buffer = makeBuff([0xF0, 0x9F, 0x98, 0x81]);
   it('creates a buffer of length 6', () => {
-    expect(buffer).toHaveLength(6);
+    expect(buffer).toHaveLength(4);
   });
   it('can get string back', () => {
-    expect(buffer.toString()).toBe('string');
+    expect(buffer.toString()).toBe('😁');
   });
 });
